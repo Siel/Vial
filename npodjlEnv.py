7d9f8228f0afbf5840eb012fb2154087eb73aa20
Julian D. Otalvaro <juliandavid347@gmail.com>
2021-03-24 18:58:56 -0700

    first 10000 run

--- npodjlEnv.py
+++ npodjlEnv.py
@@ -6,8 +6,8 @@ class NpodJLEnv:
     def __init__(self):
         self.pkdata_file = "data/data_1comp_neely.csv"
         self.sim_file = ""
-        self.a = [1, 5]
-        self.b = [2, 25]
+        self.a = [0.001, 125]
+        self.b = [2, 625]
         self.c0 = 0
         self.c1 = 0.1
         self.reward = 0
@@ -19,8 +19,8 @@ class NpodJLEnv:
         return("%f_%f_%f_%f_%f_%f" % (self.a[0], self.a[1], self.b[0], self.b[1], self.c0, self.c1))
 
     def reset(self):
-        self.a = [1, 5]
-        self.b = [2, 25]
+        self.a = [0.001, 125]
+        self.b = [2, 625]
         self.c0 = 0
         self.c1 = 0.1
         self.reward = 0
@@ -53,7 +53,7 @@ class NpodJLEnv:
         #     self.c1 = self.c1 - self.dec_rate * self.c1
 
         (cycles, theta, w, fobj, _) = npod.run(self.pkdata_file,
-                                               self.a, self.b, self.c0, self.c1, 0, 100)
+                                               self.a, self.b, self.c0, self.c1, 0, 51)
         Main.fobj = fobj
         fobj_val = Main.eval("fobj[]")
         return_dic = {'cycles': cycles, 'theta': theta, 'w': w, 'fobj': fobj_val, 'a': self.a, 'b': self.b, 'c0': self.c0,
@@ -63,20 +63,21 @@ class NpodJLEnv:
         return(return_dic)
 
     def ver(self):
-        return(0.6)
+        return(0.8)
 
     def actions(self):
         return([
+            "dec_a2",
+            "dec_b2",
             "inc_a1",
             "inc_b1",
-            "inc_a2",
-            "inc_b2",
-            # "inc_c0",
-            # "inc_c1",
             "dec_a1",
             "dec_b1",
-            "dec_a2",
-            "dec_b2"
+            "inc_a2",
+            "inc_b2"
+            # "inc_c0",
+            # "inc_c1",
             # "dec_c0",
             # "dec_c1"
+
         ])
